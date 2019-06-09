49045e207792a6e15533a4e8a1a480b8b5b5f1b1
Adriel Klein <adrielmklein@gmail.com>
2019-06-09 12:29:32 -0700

    hook up front end with backend

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Products from './Products';
-import products from './products.json';
 import Filters from './Filters';
+import axios from 'axios';
 
-const App = () => (
-  <div className="app">
-    <div className="App-row">
-      <Filters />
+const Body = ({products}) => {
+    return <>
+        <div className="App-row">
+            <Filters/>
+        </div>
+        <div className="App-row">
+            <Products products={products}/>
+        </div>
+    </>
+};
+
+const App = () => {
+    const [products, setProducts] = useState();
+    useEffect(() => {
+        async function fetchData() {
+            const result = await axios.get('/products');
+            setProducts(result.data);
+        }
+
+        fetchData();
+
+    }, []);
+
+    return <div className="app">
+        <h1>Products</h1>
+        {products ? <Body products={products}/> : 'Loading products...'}
     </div>
-    <div className="App-row">
-      <Products products={products} />
-    </div>
-  </div>
-);
+};
 
 export default App;
